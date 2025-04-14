8cb2fc8cde295b60589993f9e62577dc9ee7c117
SaCasseta <scasseta@gmail.com>
2025-04-14 13:55:01 -0300

    criado

---
+++ src/main/java/com/example/nois/NoisApplication.java
@@ -0,0 +1,13 @@
+package com.example.nois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nois/NoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
